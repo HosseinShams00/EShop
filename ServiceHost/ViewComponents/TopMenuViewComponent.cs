1d344698bcacad8891609fc1b2530f0fe64ea0be
Hossein <hossinshams1380@gmail.com>
2022-09-24 20:43:51 +0330

    Add top menu view component

---
+++ ServiceHost/ViewComponents/TopMenuViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using EShopQuery.Contracts.User.ProductCategories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.ViewComponents;
+
+public class TopMenuViewComponent : ViewComponent
+{
+    private readonly IUserProductCategoryQuery _productCategoryQuery;
+    public IReadOnlyCollection<UserProductCategoriesQuery> ViewModel;
+
+    public TopMenuViewComponent(IUserProductCategoryQuery productCategoryQuery)
+    {
+        _productCategoryQuery = productCategoryQuery;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        ViewModel = _productCategoryQuery.GetViewModels();
+        return View(ViewModel);
+    }
+
+}
